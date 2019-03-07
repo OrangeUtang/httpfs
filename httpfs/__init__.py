acb57014a9490b6dc30a0aa8b4f1bca21397c04f
OrangeUtang <Joe.dus.sen@gmail.com>
2019-03-06 23:28:11 -0500

    deleted commented out code

--- httpfs/__init__.py
+++ httpfs/__init__.py
@@ -4,8 +4,6 @@ from httpServerLib.ResponseHandler import ResponseHandler
 import click
 import os
 
-# used path to dir = D:\School_Stuff\Python_Stuff\PythonHttpfs\workdir
-
 
 @click.command()
 @click.option('-v', '--verbose', is_flag=True, help="Prints debugging messages")
